393073cc7b274300221d475de9b540c08912019c
joshrandall <jrandall@utexas.edu>
2016-01-19 10:44:43 -0600

    lighter check in

---
+++ scripts/lighter.lua
@@ -0,0 +1,42 @@
+init = function(args)
+   requestID = 0
+end
+
+request = function()
+   local base = ""
+
+   if requestID == 0 then
+      path = "/"
+      requestID = requestID + 1
+   elseif requestID == 1 then
+      path = "/styles/style.css"
+      requestID = requestID + 1
+   elseif requestID == 2 then
+      path = "/styles/highlight.css"
+      requestID = requestID + 1
+   elseif requestID == 3 then
+      path = "/styles/media.css"
+      requestID = requestID + 1
+   elseif requestID == 4 then
+      path = "/images/line-tile.png"
+      requestID = requestID + 1
+   elseif requestID == 5 then
+      path = "/images/noise.png"
+      requestID = requestID + 1
+   elseif requestID == 6 then
+      path = "/images/header-bottom.png"
+      requestID = requestID + 1
+   elseif requestID == 7 then
+      path = "/styles/fonts/source-sans-pro/SourceSansPro-Regular-webfont.woff"
+      requestID = requestID + 1
+   elseif requestID == 8 then
+      path = "/styles/fonts/source-sans-pro/SourceSansPro-Black-webfont.woff"
+      requestID = requestID + 1
+   elseif requestID == 9 then
+      path = "/favicon.ico"
+      requestID = 0
+   end
+
+   --print(path)
+   return wrk.format(nil, path)
+end
